e0bbb161e145aa276dfca86f16ad7203515ac0f3
Robert Ying <yingrui205@gmail.com>
2018-05-14 03:30:20 +0800

    Use Windows Credential Locker to save the password

---
+++ Helpers/CredentialHelper.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Security.Credentials;
+
+namespace CampusNet
+{
+    class CredentialHelper
+    {
+        public static void AddAccount(string username, string password)
+        {
+            var vault = new PasswordVault();
+
+            var credential = GetCredentialFromLocker(username);
+            if (credential == null)
+            {
+                vault.Add(new PasswordCredential("CampusNet", username, password));
+            }
+            else
+            {
+                RemoveAccount(username);
+                vault.Add(new PasswordCredential("CampusNet", username, password));
+            }
+        }
+
+        public static bool RemoveAccount(string username)
+        {
+            var vault = new PasswordVault();
+
+            try
+            {
+                var credentialList = vault.FindAllByUserName(username);
+                if (credentialList.Count > 0)
+                {
+                    var credential = credentialList.First();
+                    credential.RetrievePassword();
+                    vault.Remove(new PasswordCredential("CampusNet", credential.UserName, credential.Password));
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static PasswordCredential GetCredentialFromLocker(string username)
+        {
+            var vault = new PasswordVault();
+
+            try
+            {
+                var credentialList = vault.FindAllByUserName(username);
+                if (credentialList.Count > 0)
+                {
+                    return credentialList.First();
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
